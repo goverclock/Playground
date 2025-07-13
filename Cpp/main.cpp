0edf536e8dbbe9fb4d99f155efa6e02781813c15
goverclock <goverclock@outlook.com>
2025-07-13 11:37:26 +0800

    Cpp simple factory
    Signed-off-by: goverclock <goverclock@outlook.com>

--- Cpp/main.cpp
+++ Cpp/main.cpp
@@ -4,12 +4,54 @@
 #include <list>
 using namespace std;
 
-int main() {
-    int *pia = new int[10];
-    int *pia2 = new int[10]();
+class Operation {
+   public:
+    virtual double get_result() = 0;
+    virtual ~Operation() = default;
 
-    for(int i = 0; i < 10; i++) cout << pia[i] << endl;
-    for(int i = 0; i < 10; i++) cout << pia2[i] << endl;
-    
+    double num_a = 0.0;
+    double num_b = 0.0;
+};
+
+class AddOperation : public Operation {
+   public:
+    double get_result() override { return num_a + num_b; };
+};
+
+class SubOperation : public Operation {
+   public:
+    double get_result() override { return num_a - num_b; };
+};
+
+class OperationFactory {
+   public:
+    static unique_ptr<Operation> create_operation(char opr) {
+        Operation* ret = nullptr;
+        switch (opr) {
+            case '+':
+                ret = new AddOperation();
+                break;
+            case '-':
+                ret = new SubOperation();
+                break;
+            default:
+                // FUCK this shit
+                break;
+        }
+        return unique_ptr<Operation>(ret);
+    }
+};
+
+int main() {
+    double num1 = 0.0;
+    double num2 = 0.0;
+    cin >> num1 >> num2;
+
+    auto opr = OperationFactory::create_operation('+');
+    opr->num_a = num1;
+    opr->num_b = num2;
+
+    cout << opr->get_result();
+
     return 0;
 }
